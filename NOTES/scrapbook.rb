e83e0bece8381e92a27e3f3b6afa3150886ad6a9
Duncan Murchison <duncancmurchison@gmail.com>
2023-02-24 10:56:43 -0500

    so any options

--- NOTES/scrapbook.rb
+++ NOTES/scrapbook.rb
@@ -1,3 +1,34 @@
 # Notes and Scrapbook
 
+## Difference between procs and lambdas
+
+def proc_demo_method
+  proc_demo = Proc.new { return "Only I print!" }
+  proc_demo.call
+  "But what about me?" # Never reached
+end
+
+puts proc_demo_method 
+# Output 
+# Only I print!
+
+# (Notice that the proc breaks out of the method when it returns the value.)
+
+def lambda_demo_method
+  lambda_demo = lambda { return "Will I print?" }
+  lambda_demo.call
+  "Sorry - it's me that's printed."
+end
+
+puts lambda_demo_method
+# Output
+# Sorry - it's me that's printed.
+
+# (Notice that the lambda returns back to the method in order to complete it.)
+
+
+# ------------------------------------------------------------------
+
+
+
 
